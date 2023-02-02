da70b0b16fb16fb8b0fb8baf7db62ffc5a3d3a60
hanriotr <Robin.Hanriot@grenoble-inp.org>
2023-02-02 16:36:04 +0100

    memory clean

--- src/PerformanceOption.cpp
+++ src/PerformanceOption.cpp
@@ -16,5 +16,7 @@ double PerformanceOption::payoff(const PnlVect *spots)
 {
     PnlVect *prod = pnl_vect_copy(spots);
     pnl_vect_mult_vect_term(prod, lambda_);
-    return max(pnl_vect_max(prod) - strike_, 0.0);
+    double payoff = max(pnl_vect_max(prod) - strike_, 0.0);
+    pnl_vect_free(&prod);
+    return payoff;
 }
