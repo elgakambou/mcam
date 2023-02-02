a0150fba5135a509497767d6dba6c451495864ff
hanriotr <Robin.Hanriot@grenoble-inp.org>
2023-02-02 16:35:04 +0100

    memory clean

--- src/MonteCarlo.cpp
+++ src/MonteCarlo.cpp
@@ -59,6 +59,7 @@ double MonteCarlo :: price(double T, int nbDates, PnlRng* rng)
                 pnl_vect_set(taus_k_1, l, (double) k * timeStep);
         }
     }
+    
     double tau1, payoff=0.0;
     for (int l=0; l<nbSamples; l++)
     {
@@ -67,5 +68,14 @@ double MonteCarlo :: price(double T, int nbDates, PnlRng* rng)
         pnl_mat_get_row(S_tau_k_l_1, path_l, (int) (tau1 / timeStep));
         payoff += exp(-mod_->r_ * tau1) * opt_->payoff(S_tau_k_l_1);
     }
+
+    pnl_vect_free(&taus_k_1);
+    pnl_vect_free(&dataY);
+    pnl_vect_free(&coeff);
+    pnl_vect_free(&S_tk_l);
+    pnl_vect_free(&S_tau_k_l_1);
+    pnl_mat_free(&dataX);
+    pnl_array_free(&paths);
+
     return std::max(opt_->payoff(mod_->spot_), payoff / (double) nbSamples);
 }
